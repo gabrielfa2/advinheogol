cf9b347535373bc41453674e93b91fc627f14fb7
Gabriel <eugabrielfa07@gmail.com>
2025-08-12 11:29:00 -0300

    Updated gameLogic.js

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -48,7 +48,7 @@ class FootballQuizGame {
         this.countdownTimerModal = document.getElementById('countdownTimer');
         
         // Define a intensidade máxima do blur em pixels
-        this.maxBlur = 16; 
+        this.maxBlur = 10; 
         
         this.init();
     }
